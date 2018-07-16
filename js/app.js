51829a10a412b9ba79fe4b4b3265aaddb57357a2
Savoir <savoir.cn@gmail.com>
2018-07-15 20:46:32 -0700

    increment the move counter and display

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ document.addEventListener("DOMContentLoaded", createCard, true);
 
 const cards= document.querySelectorAll('.card')
 const deck = document.querySelector('.deck')
-const openCard=[];
-const matchedCards=0;
+let openCard=[];
+let matchedCards=0;
+let move = 0;
 
 
 /*
@@ -29,8 +30,6 @@ function shuffle(array) {
     return array;
 }
 
-
-
 // - loop through each card and create its HTML
 // - add each card's HTML to the page
 function createCard() {
@@ -41,11 +40,19 @@ function createCard() {
         card.classList.remove("show", "open", "match");
         deck.appendChild(card);
     }
+    move = 0;
 }
 
+function changemove(){
+    if (move<2) {
+        document.querySelector('.moves').innerText= move + ' Move';
+    }
+    if (move>=2) {
+        document.querySelector('.moves').innerText= move + ' Moves';
+    }
+}
 //add restart addEventListener
 document.querySelector('.restart').addEventListener('click',createCard,true);
-
 
 const flipcard=function (event) {
     const clickedCard = event.target;
@@ -55,21 +62,25 @@ const flipcard=function (event) {
          // add the card to a *list* of "open" cards
          openCard.push(clickedCard);
      }
+     //if the list already has another card, check to see if the two cards match
      if (openCard.length === 2) {
-         //check if the cards match
          if (openCard[0].firstElementChild.className === openCard[1].firstElementChild.className ) {
              openCard.forEach(function(card){
                  card.classList.add('match')
              });
              matchedCards = matchedCards + 2;
-         } else {
-             openCard.forEach(function(card){
-                 card.classList.remove('open' ,'show')
-             })
          }
-         openCard=[];
+         //if the cards do not match, remove the cards from the list and hide the card's symbol
+         else {
+             openCard.forEach(function(card){
+                 setTimeout(function(){card.classList.remove('open' ,'show')
+             },650);
+             });
+         }
+         openCard.splice(0,openCard.length);
      }
-
+     move += 1;
+     changemove();
  };
 
 // set up the event listener for a card. If a card is clicked:
@@ -80,8 +91,7 @@ for (let i=0; i<=16; i++) {
 
 /*
  *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + if the cards do match, lock the cards in the open position
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
